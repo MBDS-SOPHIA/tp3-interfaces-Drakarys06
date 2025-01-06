cdf9e4618c11ec219ff043d9b5a60e9f9fb35c7d
thomas choubrac <thomas.choubrac06@orange.fr>
2025-01-06 10:26:33 +0100

    Etape 2 - add number

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton: Button = findViewById(R.id.button)
+        val targetNumberEditText: EditText = findViewById(R.id.targetNumber)
+
+        targetNumberEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                rollButton.isEnabled = !s.isNullOrEmpty()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
         rollButton.setOnClickListener { rollDice() }
     }
 
@@ -20,14 +35,23 @@ class MainActivity : AppCompatActivity() {
         val dice2 = Dice(6)
         val diceRoll1 = dice1.roll()
         val diceRoll2 = dice2.roll()
+        val sum = diceRoll1 + diceRoll2
 
         val resultTextView1: TextView = findViewById(R.id.textView)
         val resultTextView2: TextView = findViewById(R.id.textView2)
+        val targetNumberEditText: EditText = findViewById(R.id.targetNumber)
+
         resultTextView1.text = diceRoll1.toString()
         resultTextView2.text = diceRoll2.toString()
 
-        if (diceRoll1 == diceRoll2) {
-            Toast.makeText(this, "Félicitations ! Vous avez gagné !", Toast.LENGTH_SHORT).show()
+        val targetNumber = targetNumberEditText.text.toString().toIntOrNull()
+
+        if (targetNumber != null) {
+            if (sum == targetNumber) {
+                Toast.makeText(this, "Félicitations ! Vous avez gagné !", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Désolé, vous avez perdu. Essayez encore !", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
